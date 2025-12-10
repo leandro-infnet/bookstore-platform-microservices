372f7f40d1ae12e033a2a4f23c4c8833b08683eb
LeandroMedvedev <medvedevleandro@outlook.com>
2025-12-09 23:26:36 -0300

    chore: initial project structure scaffolding

---
+++ shop-service/src/test/java/br/com/bookstore/shop/ShopServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bookstore.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
